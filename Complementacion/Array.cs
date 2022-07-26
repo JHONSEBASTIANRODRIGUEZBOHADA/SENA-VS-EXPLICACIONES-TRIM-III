3b4833f4d698168d072a6f34c808e9d8bf14b443
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-26 12:08:22 -0500

    Add files via upload

---
+++ Complementacion/Array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementacion
+{
+    internal class Array
+    {
+        /*
+        public static void Main(string[] args)
+        {
+            //ACCEDER A ARRAY
+            Console.WriteLine("ACCEDER A ARRAY");
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars[0]);
+
+            //CAMBIAR UN ELEMENTO DEL ARREGLO
+            Console.WriteLine("CAMBIAR UN ELEMENTO DEL ARREGLO");
+            string[] cars1 = { "Volvo", "BMW", "Ford", "Mazda" };
+            cars[0] = "Opel";
+            Console.WriteLine(cars[0]);
+
+            //LONGITUD DEL ARREGLO
+
+            //BLUCLE A TRAVES DE UN ARREGLO
+            Console.WriteLine("BLUCLE A TRAVES DE UN ARREGLO");
+            string[] cars2 = { "Volvo", "BMW", "Ford", "Mazda" };
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars2[i]);
+            }
+
+            //BUCLE FOREACH
+            Console.WriteLine("BUCLE FOREACH");
+            string[] cars3= { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach(string i in cars3)
+            {
+                Console.WriteLine(cars3[i]);
+            }
+
+            //ORDENAR ARREGLOS
+            Console.WriteLine("ORDENAR ARREGLOS");
+            string[] cars4 = { "Volvo", "BMW", "Ford", "Mazda" };
+            Array.sort(cars);
+            foreach(string i in cars4)
+            {
+                Console.WriteLine(i);
+            }
+        }*/
+    }
+}
---
+++ Complementacion/Metodos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complementacion
+{
+    internal class Metodos
+    {
+        public static void Main(string[] args)
+        {
+            PrimerMetodo();
+            SegundoMetodo("\nLiam");
+            SegundoMetodo("Jenny");
+            SegundoMetodo("Anja");
+            TercerMetodo("\nSweden");
+            TercerMetodo("India");
+            TercerMetodo();
+            TercerMetodo("USA");
+            CuartoMetodo("\nLiam", 5);
+            CuartoMetodo("Jenny", 8);
+            CuartoMetodo("Anja", 31);
+            Console.WriteLine($"\n{QuintoMetodo(3)}");
+            int z = SextoMetodo(5,3);
+            Console.WriteLine(z);
+            PrimerEjercicio();
+
+        }
+
+        public static void PrimerMetodo()
+        {
+            Console.WriteLine("Linea ejecutada");
+        } 
+        
+        public static void SegundoMetodo(string fname)
+        {
+            Console.WriteLine($"{fname} Apellido");
+        }
+
+        public static void TercerMetodo(string Country = "Noruega")
+        {
+            Console.WriteLine(Country);
+        }
+
+        public static void CuartoMetodo(string fname, int age)
+        {
+            Console.WriteLine($"{fname} tiene {age} años");
+        }
+
+        public static int QuintoMetodo(int x)
+        {
+            return 5 + x;
+        }
+
+        public static int SextoMetodo(int x,int y)
+        {
+            return x + y;
+        }
+
+        public static void PrimerEjercicio()
+        {
+            string[] Nombres = new string[5];
+            int[] Edades = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese su nombre");
+                Nombres[i] = Console.ReadLine();
+                Console.WriteLine("Ingrese sun edad");
+                Edades[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < Nombres.Length; i++)
+            {
+                if (Edades[i] >= 18)
+                {
+                    Console.WriteLine($"\nEl nombre del mayor de edad es : {Nombres[i]}");
+                }
+            }
+        }
+    }
+}
