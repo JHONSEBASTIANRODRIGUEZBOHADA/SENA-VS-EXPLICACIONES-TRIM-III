25eb84e8441447efeb55296c7cce52d6a1ca76a7
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-08-04 09:47:49 -0500

    Add files via upload

---
+++ Polimorfismo/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Animal
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("El animal hace sonidos");
+        }
+    }
+
+    internal class ping : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("El cerdo hace: wee wee");
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("El perro hace: bow bow");
+        }
+    }
+}
---
+++ Polimorfismo/PrincipalMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class PrincipalMain
+    {
+        public static void Main(string[] args)
+        {
+            Animal sonidos = new Animal();
+            sonidos.animalSound();
+        }
+    }
+}
